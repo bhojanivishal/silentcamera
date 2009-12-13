f30c7e2a9a622f4e2a57c09650b1876afa9441a1
zegenvs <zegenvs@b9152192-e7dd-11de-88ac-396b0f65d9f6>
2009-12-13 21:46:35 +0000

    v1.0.1

--- trunk/gen/la/zeg/android/silentcamera/R.java
+++ trunk/gen/la/zeg/android/silentcamera/R.java
@@ -16,8 +16,6 @@ public final class R {
     public static final class id {
         public static final int camera_view=0x7f060000;
         public static final int menu_about=0x7f060003;
-        public static final int menu_cancel=0x7f060005;
-        public static final int menu_save=0x7f060004;
         public static final int menu_take_a_picture=0x7f060002;
         public static final int preview=0x7f060001;
     }
@@ -28,23 +26,23 @@ public final class R {
     }
     public static final class menu {
         public static final int camera=0x7f050000;
-        public static final int preview=0x7f050001;
     }
     public static final class string {
         public static final int about_1_0_0=0x7f040003;
+        public static final int about_1_0_1=0x7f040004;
         public static final int about_description=0x7f040002;
         public static final int config_app_name=0x7f040001;
         public static final int config_default_language=0x7f040000;
-        public static final int error_message=0x7f040006;
-        public static final int error_title=0x7f040005;
-        public static final int menu_about=0x7f04000b;
-        public static final int menu_cancel=0x7f04000e;
-        public static final int menu_save=0x7f04000d;
-        public static final int menu_take_a_picture=0x7f04000c;
-        public static final int preview_message=0x7f040008;
-        public static final int preview_ng=0x7f04000a;
-        public static final int preview_ok=0x7f040009;
-        public static final int preview_title=0x7f040007;
-        public static final int saving=0x7f040004;
+        public static final int error_message=0x7f040007;
+        public static final int error_title=0x7f040006;
+        public static final int menu_about=0x7f04000c;
+        public static final int menu_cancel=0x7f04000f;
+        public static final int menu_save=0x7f04000e;
+        public static final int menu_take_a_picture=0x7f04000d;
+        public static final int preview_message=0x7f040009;
+        public static final int preview_ng=0x7f04000b;
+        public static final int preview_ok=0x7f04000a;
+        public static final int preview_title=0x7f040008;
+        public static final int saving=0x7f040005;
     }
 }
